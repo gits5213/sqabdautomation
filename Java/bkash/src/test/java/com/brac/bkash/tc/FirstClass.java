145b7726f8ae45d960c273b9c2d1429c0ecd53ce
md shahnewaz zaman <mzaman@resqsoft.com>
2021-04-08 22:29:45 -0400

    Java Maven Project added

---
+++ Java/bkash/src/test/java/com/brac/bkash/tc/FirstClass.java
@@ -0,0 +1,7 @@
+package com.brac.bkash.tc;
+
+class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("We are learning Java at IT Training & Consultant Inc.");
+    }
+}
