d4ec34893657b00cf74938e6d85331356a20f6da
Amit saha <alexiamitsaha@gmail.com>
2021-04-15 01:14:41 +0600

    Java variable & data types

---
+++ Java/amit/nagad/src/test/java/com/bpo/nagad/basicJava/JavaVariable.java
@@ -0,0 +1,28 @@
+package com.bpo.nagad.basicJava;
+
+public class JavaVariable {
+
+        //instance variable
+        int A = 6;
+        //static variable
+        static int C= 7;
+
+    public static void main(String[] args) {
+        //variable
+        // Variable is a container which hold the value
+        // Variable is a name of memory location of JVM
+        // 1. Local 2. instance 3. static
+        // 2 types of Data types
+        // 1. primitive 2. Non-primitive
+
+        int B =5;
+        System.out.println(B);
+        String learing = "Java";
+        System.out.println(learing);
+
+
+
+
+
+    }
+}
