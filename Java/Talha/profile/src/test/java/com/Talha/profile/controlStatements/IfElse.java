bc950830b89bf73353015c950acc2e4fac9bb338
TalhaBinAshraf1 <talhabinashraf@gmail.com>
2021-06-01 10:55:39 +0600

    HTML Code Changed

---
+++ Java/Talha/profile/src/test/java/com/Talha/profile/controlStatements/IfElse.java
@@ -0,0 +1,11 @@
+package com.Talha.profile.controlStatements;
+
+public class IfElse {
+    public static void main(String[] args) {
+
+        int x=5;
+        int y= 3;
+
+        System.out.println( x+y);
+    }
+}
